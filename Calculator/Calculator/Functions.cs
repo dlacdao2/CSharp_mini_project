188497f43c932c8e78614e2ff82e4f20232744aa
Donwin Lacdao <dlacdao2@academic.RRC.CA>
2024-02-23 14:46:15 -0600

    Initial Input .cs created

---
+++ Calculator/Calculator/Functions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Functions
+    {
+        public List<string> calculateTotalValue()
+        {
+
+        }
+
+
+
+
+    }
+}
+
+/*
+ + 	(plus)
+-	(minus)
+=	(executeCalculate)
+X	(times)
+%	(divide)
+C	(clear)
+!	(factorial)
+ */
+
+
---
+++ Calculator/Calculator/Input.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Input
+    {
+        List<string> list = new List<string>();
+
+        public void gettingInput()
+        {
+            string input = "0";
+            while (true)
+            {
+                Console.WriteLine("Enter values:");
+                input = Console.ReadLine();
+
+                if (input == "=")
+                {
+                    foreach (string element in list)
+                    {
+                        Console.WriteLine(element);
+                    }
+                    Console.WriteLine("this loop is ended");
+                    // break;
+                }
+                else if (input == "+" || input == "-" || input == "*" || input == "%")
+                {
+                    list.Add(input);
+                }
+                else
+                {
+                    list.Add(input);
+                }
+            }
+
+
+        }
+
+
+    }
+
+}
